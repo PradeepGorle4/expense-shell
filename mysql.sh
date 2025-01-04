7fd737a929a29abb603f017ed74d2f559d9c919d
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-01-04 12:31:43 +0530

    Expense-project

--- mysql.sh
+++ mysql.sh
@@ -35,13 +35,13 @@ validate() {
 
 echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_Name
 
-dnf install mysql-server -y $TIMESTAMP" &>>$LOG_FILE_Name
+dnf install mysql-server -y $TIMESTAMP &>>$LOG_FILE_Name
 validate "Installing mysql-server"
 
-systemctl enable mysqld $TIMESTAMP" &>>$LOG_FILE_Name
+systemctl enable mysqld $TIMESTAMP &>>$LOG_FILE_Name
 validate "Enabling mysql-server"
 
-systemctl start mysqld $TIMESTAMP" &>>$LOG_FILE_Name
+systemctl start mysqld $TIMESTAMP &>>$LOG_FILE_Name
 validate "Starting mysql-server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 $TIMESTAMP" &>>$LOG_FILE_Name
